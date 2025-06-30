ee6fba3aa947998cd3a603ac956f4e0663121468
rafael.depontes <rafael.depontes@dataprev.gov.br>
2025-06-30 18:01:38 -0300

    style: removing empty dependencies

--- front/app/(main)/homepage/page.tsx
+++ front/app/(main)/homepage/page.tsx
@@ -1,10 +1,6 @@
 "use client";
 import React from "react";
 import { Button } from "@mui/material";
-import { ApiService } from "@services/ApiService";
-import { Button } from "@mui/material";
-import { AppContext } from "@components/context/AppContext";
-import { Api } from "@mui/icons-material";
 
 export default function HomePage() {
   return (
